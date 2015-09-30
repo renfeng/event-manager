ea0c4c8d731e8937d1aea388a4f28ba21b2dfe0a
Frank R <renfeng.cn@gmail.com>
2015-09-30 17:07:53 +0800

    .

--- gae-default/src/main/java/com/google/developers/event/qrcode/ParticipantsServlet.java
+++ gae-default/src/main/java/com/google/developers/event/qrcode/ParticipantsServlet.java
@@ -2,10 +2,15 @@ package com.google.developers.event.qrcode;
 
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonGenerator;
+import com.google.developers.api.CellFeedProcessor;
 import com.google.developers.api.SpreadsheetManager;
 import com.google.developers.event.ActiveEvent;
 import com.google.developers.event.RegisterFormResponseSpreadsheet;
 import com.google.developers.event.http.Path;
+import com.google.gdata.data.batch.BatchOperationType;
+import com.google.gdata.data.batch.BatchUtils;
+import com.google.gdata.data.spreadsheet.CellEntry;
 import com.google.gdata.util.ServiceException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -17,6 +22,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by frren on 2015-09-29.
@@ -41,7 +49,7 @@ public class ParticipantsServlet extends HttpServlet
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(final HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		/*
 		 * retrieve event id from http header, referer
@@ -73,8 +81,80 @@ public class ParticipantsServlet extends HttpServlet
 		} catch (ServiceException e) {
 			throw new ServletException(e);
 		}
+
+		String registerURL = activeEvent.getRegisterResponsesURL();
+		if (registerURL == null) {
+			throw new ServletException(
+					"Missing URL to the register form responses of event, " + activeEvent.getEvent());
+		}
+
+		final String registerEmailColumn = activeEvent.getRegisterEmailColumn();
+		if (registerEmailColumn == null) {
+			throw new ServletException(
+					"Missing emailAddress column mapping for the register form responses of event, " +
+							activeEvent.getEvent());
+		}
+
+		final String registerNameColumn = activeEvent.getRegisterNameColumn();
+		if (registerNameColumn == null) {
+			throw new ServletException(
+					"Missing nickname column mapping for the register form responses of event, " +
+							activeEvent.getEvent());
+		}
+
 		/*
 		 * TODO list participants
 		 */
+
+		/*
+		 * headers must be set before body
+		 *
+		 * java - Response encoding of Google App Engine (can not change response encoding) - Stack Overflow
+		 * http://stackoverflow.com/a/9447068/333033
+		 */
+		resp.setContentType("text/javascript; charset=utf-8");
+
+		final JsonGenerator generator = jsonFactory.createJsonGenerator(resp.getWriter());
+		generator.writeStartObject();
+		generator.writeFieldName("participants");
+		generator.writeStartArray();
+
+		CellFeedProcessor cellFeedProcessor = new CellFeedProcessor(spreadsheetManager) {
+
+			@Override
+			protected boolean processDataRow(Map<String, String> valueMap, URL cellFeedURL)
+					throws IOException, ServiceException {
+
+				generator.writeStartObject();
+
+				generator.writeFieldName("nick");
+				generator.writeString(valueMap.get(registerNameColumn));
+
+				generator.writeFieldName("email");
+				generator.writeString(valueMap.get(registerEmailColumn));
+
+				generator.writeFieldName("sent");
+				String qrCode = valueMap.get(QR_CODE_COLUMN);
+				if (qrCode != null && !qrCode.equals("x")) {
+					generator.writeBoolean(true);
+				} else {
+					generator.writeBoolean(false);
+				}
+
+				generator.writeEndObject();
+
+				return true;
+			}
+		};
+		try {
+			cellFeedProcessor.processForBatchUpdate(spreadsheetManager.getWorksheet(registerURL),
+					registerNameColumn, registerEmailColumn, QR_CODE_COLUMN);
+		} catch (ServiceException e) {
+			throw new ServletException(e);
+		}
+
+		generator.writeEndArray();
+		generator.writeEndObject();
+		generator.flush();
 	}
 }
