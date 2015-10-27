d5e44e322bc747b211180afc0083a7c4bc5da4c2
Frank R <renfeng.cn@gmail.com>
2015-10-28 07:49:59 +0800

    .

---
+++ shared/src/test/java/com/google/developers/GHSHunt.java
@@ -0,0 +1,32 @@
+package com.google.developers;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by renfeng on 10/28/15.
+ */
+public class GHSHunt {
+
+	public static void main(String... args) throws UnknownHostException {
+		if (args.length == 0) {
+			return;
+		}
+		String host = args[0];
+		List<String> hostAddressList = new ArrayList<>();
+		Security.setProperty("networkaddress.cache.ttl", "0");
+		while (true) {
+			InetAddress address = InetAddress.getByName(host);
+			String hostAddress = address.getHostAddress();
+			if (hostAddressList.contains(hostAddress)) {
+				continue;
+			}
+			System.out.println(hostAddress + " " + new Date());
+			hostAddressList.add(hostAddress);
+		}
+	}
+}
